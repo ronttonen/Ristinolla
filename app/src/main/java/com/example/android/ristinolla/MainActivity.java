cd9f551a16a759c3b10e0b67047ef87802ba8d6b
ronttonen <roni.p.ahti@gmail.com>
2017-03-24 14:26:09 +0200

    korjatturuudukko

--- app/src/main/java/com/example/android/ristinolla/MainActivity.java
+++ app/src/main/java/com/example/android/ristinolla/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android.ristinolla;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.R.attr.button;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void buttonClick(View v)
+    {
+        TextView tv = (TextView)findViewById(R.id.center);
+        tv.setText("X");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
