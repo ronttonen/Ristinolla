00661a474ea8a83ed682baf8a318a62cc5a6f9af
Miska Aaltonen <miska.aaltonen@fiarone.com>
2017-03-29 10:19:05 +0300

    Fixing Ai

--- app/src/main/java/com/example/android/ristinolla/Computer.java
+++ app/src/main/java/com/example/android/ristinolla/Computer.java
@@ -34,33 +34,39 @@ public class Computer extends AppCompatActivity {
     }
 
     Random rnd = new Random();
-    int board[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int color = Color.argb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-    int turn = rnd.nextInt(2);
+    int board[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; // 0 = empty , 1 = player1, 2 = bot / player2, 3 = locked
 
+    // pick random background
+    int color = Color.argb(rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    int turn = 0; // set X to start
 
 
 
 
 
     public void markSquare(View v) {
+        // get target
         TextView tv = (TextView) findViewById(v.getId());
 
-
+        // check if box is empty (ischecked = 0 = empty)
         if (turn == 0 && isChecked(v.getId()) == 0) {
             tv.setText("X");
             checkSquare(v.getId());
             switchTurn();
             checkWin();
             ai();
-            switchTurn();
-            checkWin();
+
+            if (!win){
+                switchTurn();
+                checkWin();
+            }
+
 
 
         }
     }
 
-
+    // check if box is checked, return board[value] or 0
     public int isChecked(int x) {
         if (x == R.id.center) {
             return board[0];
@@ -107,35 +113,28 @@ public class Computer extends AppCompatActivity {
                 case 0:
                     board[0] = 1;
                     break;
-                case 1:
-                    board[0] = 2;
+
             }
         } else if (x == R.id.left) {
             switch (turn) {
                 case 0:
                     board[1] = 1;
                     break;
-                case 1:
-                    board[1] = 2;
-                    break;
+
             }
         } else if (x == R.id.right) {
             switch (turn) {
                 case 0:
                     board[2] = 1;
                     break;
-                case 1:
-                    board[2] = 2;
-                    break;
+
             }
         } else if (x == R.id.topLeft) {
             switch (turn) {
                 case 0:
                     board[3] = 1;
                     break;
-                case 1:
-                    board[3] = 2;
-                    break;
+
             }
         } else if (x == R.id.topRight) {
             switch (turn) {
@@ -151,41 +150,33 @@ public class Computer extends AppCompatActivity {
                 case 0:
                     board[5] = 1;
                     break;
-                case 1:
-                    board[5] = 2;
-                    break;
+
             }
         } else if (x == R.id.bottomLeft) {
             switch (turn) {
                 case 0:
                     board[6] = 1;
                     break;
-                case 1:
-                    board[6] = 2;
-                    break;
+
             }
         } else if (x == R.id.bottomRight) {
             switch (turn) {
                 case 0:
                     board[7] = 1;
                     break;
-                case 1:
-                    board[7] = 2;
-                    break;
+
             }
         } else if (x == R.id.bottom) {
             switch (turn) {
                 case 0:
                     board[8] = 1;
                     break;
-                case 1:
-                    board[8] = 2;
-                    break;
+
             }
 
         }
     }
-
+    // lock boxes
     public void checkAll() {
         for (int i = 0; i < board.length; i++) {
             board[i] = 3;
@@ -194,104 +185,104 @@ public class Computer extends AppCompatActivity {
     }
 
     public void ai() {
-        int target = 3;
-
-        do {
-            target = rnd.nextInt(9);
-        }
-        while (board[target] != 0 || (board[0] != 0 && board[1] != 0 && board[2] != 0 && board[3] != 0 && board[4] != 0
-                && board[5] != 0 && board[6] != 0 && board[7] != 0 && board[8] != 0) || (win == true));
-        if (target == 0) {
-            TextView ff = (TextView) findViewById(R.id.center);
-            if (turn == 1) {
-                ff.setText("O");
-                board[0] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[0] = 1;
+        int target;
+        if (!win) {
+            do {
+                target = rnd.nextInt(9); // get random number 0-9 WOW SO WISE AI
             }
+            while (board[target] != 0);
+            if (target == 0) {
+                TextView ff = (TextView) findViewById(R.id.center);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[0] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[0] = 1;
+                }
 
-        } else if (target == 1) {
-            TextView ff = (TextView) findViewById(R.id.left);
-            if (turn == 1) {
-                ff.setText("O");
-                board[1] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[1] = 1;
+            } else if (target == 1) {
+                TextView ff = (TextView) findViewById(R.id.left);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[1] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[1] = 1;
+                }
+
+            } else if (target == 2) {
+                TextView ff = (TextView) findViewById(R.id.right);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[2] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[2] = 1;
+                }
+
+            } else if (target == 3) {
+                TextView ff = (TextView) findViewById(R.id.topLeft);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[3] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[3] = 1;
+                }
+
+            } else if (target == 4) {
+                TextView ff = (TextView) findViewById(R.id.topRight);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[4] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[4] = 1;
+                }
+
+            } else if (target == 5) {
+                TextView ff = (TextView) findViewById(R.id.top);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[5] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[5] = 1;
+                }
+
+            } else if (target == 6) {
+                TextView ff = (TextView) findViewById(R.id.bottomLeft);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[6] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[6] = 1;
+                }
+
+            } else if (target == 7) {
+                TextView ff = (TextView) findViewById(R.id.bottomRight);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[7] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[7] = 1;
+                }
+
+            } else if (target == 8) {
+                TextView ff = (TextView) findViewById(R.id.bottom);
+                if (turn == 1) {
+                    ff.setText("O");
+                    board[8] = 2;
+                } else if (turn == 0) {
+                    ff.setText("X");
+                    board[8] = 1;
+                }
+
+
             }
-
-        } else if (target == 2) {
-            TextView ff = (TextView) findViewById(R.id.right);
-            if (turn == 1) {
-                ff.setText("O");
-                board[2] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[2] = 1;
-            }
-
-        } else if (target == 3) {
-            TextView ff = (TextView) findViewById(R.id.topLeft);
-            if (turn == 1) {
-                ff.setText("O");
-                board[3] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[3] = 1;
-            }
-
-        } else if (target == 4) {
-            TextView ff = (TextView) findViewById(R.id.topRight);
-            if (turn == 1) {
-                ff.setText("O");
-                board[4] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[4] = 1;
-            }
-
-        } else if (target == 5) {
-            TextView ff = (TextView) findViewById(R.id.top);
-            if (turn == 1) {
-                ff.setText("O");
-                board[5] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[5] = 1;
-            }
-
-        } else if (target == 6) {
-            TextView ff = (TextView) findViewById(R.id.bottomLeft);
-            if (turn == 1) {
-                ff.setText("O");
-                board[6] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[6] = 1;
-            }
-
-        } else if (target == 7) {
-            TextView ff = (TextView) findViewById(R.id.bottomRight);
-            if (turn == 1) {
-                ff.setText("O");
-                board[7] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[7] = 1;
-            }
-
-        } else if (target == 8) {
-            TextView ff = (TextView) findViewById(R.id.bottom);
-            if (turn == 1) {
-                ff.setText("O");
-                board[8] = 2;
-            } else if (turn == 0) {
-                ff.setText("X");
-                board[8] = 1;
-            }
-
-
         }
     }
 
